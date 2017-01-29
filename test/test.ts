6400e0f03839c30ef4a3e701e48be7ca33bc7dde
Phil Kunz <gitlab@philkunz.com>
2017-01-29 17:45:48 +0100

    fix testing timeouts

--- test/test.ts
+++ test/test.ts
@@ -14,7 +14,7 @@ describe('cflare', function () {
     describe('.CflareAccount', function () {
         describe('.listZones()', function () {
             it('should display an entire account', function (done) {
-                this.timeout(60000)
+                this.timeout(600000)
                 testCflareAccount.listZones()
                     .then((responseArg) => {
                         done()
@@ -23,7 +23,7 @@ describe('cflare', function () {
         })
         describe('.getZoneId(domainName)', function () {
             it('should get an Cloudflare Id for a domain string', function (done) {
-                this.timeout(60000)
+                this.timeout(600000)
                 testCflareAccount.getZoneId('bleu.de')
                     .then((responseArg) => {
                         done()
@@ -32,7 +32,7 @@ describe('cflare', function () {
         })
         describe('.listRecords(domainName)', function () {
             it('should list all records for a specific Domain Name', function (done) {
-                this.timeout(60000)
+                this.timeout(600000)
                 testCflareAccount.listRecords('bleu.de')
                     .then((responseArg) => {
                         console.log(responseArg)
@@ -42,14 +42,14 @@ describe('cflare', function () {
         })
         describe('.createRecord', function () {
             it('should create a valid record for a level 2 domain', function (done) {
-                this.timeout(60000)
+                this.timeout(600000)
                 testCflareAccount.createRecord('bleu.de', 'A', '127.0.0.1')
                     .then(function (responseArg) {
                         done()
                     })
             })
             it('should create a valid record for a subdomain', function (done) {
-                this.timeout(60000)
+                this.timeout(600000)
                 testCflareAccount.createRecord('subdomain.bleu.de', 'A', '127.0.0.1')
                     .then(function (responseArg) {
                         done()
@@ -58,7 +58,7 @@ describe('cflare', function () {
         })
         describe('.getRecord', function () {
             it('should get a record from Cloudflare', function (done) {
-                this.timeout(60000)
+                this.timeout(600000)
                 testCflareAccount.getRecord('bleu.de', 'A')
                     .then(function (responseArg) {
                         console.log(responseArg)
@@ -68,7 +68,7 @@ describe('cflare', function () {
         })
         describe('.removeRecord', function () {
             it('should remove a record from Cloudflare', function (done) {
-                this.timeout(60000)
+                this.timeout(600000)
                 testCflareAccount.removeRecord('bleu.de', 'A')
                     .then(function (responseArg) {
                         console.log(responseArg)
@@ -76,7 +76,7 @@ describe('cflare', function () {
                     })
             })
             it('should remove a subdomain record from Cloudflare', function (done) {
-                this.timeout(60000)
+                this.timeout(600000)
                 testCflareAccount.removeRecord('subdomain.bleu.de', 'A')
                     .then(function (responseArg) {
                         console.log(responseArg)
